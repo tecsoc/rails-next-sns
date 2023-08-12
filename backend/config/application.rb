251c83471ea1791749db0f2b390bbdfc2c68054e
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-08-12 23:44:29 +0900

    graphql setup

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -14,6 +14,8 @@ require "action_view/railtie"
 # require "action_cable/engine"
 require "rails/test_unit/railtie"
 
+require "sprockets/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -34,6 +36,8 @@ module Backend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.api_only = true
   end
 end
